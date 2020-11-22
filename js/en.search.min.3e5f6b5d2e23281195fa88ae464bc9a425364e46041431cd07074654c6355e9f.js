55f8bc345def997b20feb7bd35cb0c279e82fd2d
Hugo Vantighem <hugo.vantighem2@altran.com>
2020-11-22 12:15:14 +0100

    remove geedoc site.

--- js/en.search.min.3e5f6b5d2e23281195fa88ae464bc9a425364e46041431cd07074654c6355e9f.js
+++
@@ -1,7 +0,0 @@
-'use strict';(function(){const input=document.querySelector('#gdoc-search-input');const results=document.querySelector('#gdoc-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/js/flexsearch.min.js');loadScript('/js/en.search-data.min.f1efecac6f12c341bf86ef1daed6f1ac646fba212cd0dd4cbbbcf789223938dc.js',function(){input.required=false;search();});}
-function search(){while(results.firstChild){results.removeChild(results.firstChild);}
-if(!input.value){console.log("empty")
-results.classList.remove("has-hits");return;}
-const searchHits=window.geekdocSearchIndex.search(input.value,10);console.log(searchHits.length);if(searchHits.length>0){results.classList.add("has-hits");}else{results.classList.remove("has-hits");}
-searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);results.classList.add("DUMMY");});}
-function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
